4e91f242fababe9bf8175a1805ee28aa5c66a730
Veronika <veronikayatskova03@gmail.com>
2023-07-24 12:18:19 +0300

    add folder's infrastructure

---
+++ Profiles/Profiles.Domain/Entities/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Profiles.Domain.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = default!;
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+    }
+}
