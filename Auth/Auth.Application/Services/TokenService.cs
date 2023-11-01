432f648c01d090f9b15cd63f0337376decb04b0e
Veronika <veronikayatskova03@gmail.com>
2023-11-01 15:25:57 +0300

    add nginx server set up

--- Auth/Auth.Application/Services/TokenService.cs
+++ Auth/Auth.Application/Services/TokenService.cs
@@ -11,6 +11,7 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using Auth.Domain.Exceptions;
 
 namespace Auth.Application.Services
 {
